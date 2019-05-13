30ca5ea4f4a9ff959b47e296b1eb976ce99525d3
Olena Rostotskyy <olenazachkow@gmail.com>
2019-05-13 11:25:12 -0700

    delete favorites

--- components/FavoriteComponent.js
+++ components/FavoriteComponent.js
@@ -4,13 +4,23 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
+
+
+
 
 const mapStateToProps = state => {
     return {
       dishes: state.dishes,
       favorites: state.favorites
     }
-  }
+}
+
+const mapDispatchToProps = dispatch => ({
+    deleteFavorite: (dishId) => dispatch(deleteFavorite(dishId))
+});
+
 
 class Favorites extends Component {
 
@@ -21,20 +31,31 @@ class Favorites extends Component {
     render() {
 
         const { navigate } = this.props.navigation;
-        
+    
         const renderMenuItem = ({item, index}) => {
     
+            const rightButton = [
+                {
+                    text: 'Delete', 
+                    type: 'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ];
             return (
-                <ListItem
-                    key={index}
-                    title={item.name}
-                    subtitle={item.description}
-                    onPress={() => navigate('DishDetail', { dishId: item.id })}
-                    leftAvatar={{ source: {uri: baseUrl + item.image}}}
+                //means that when you click on a button, then the Swipeout which reveals option buttons will be automatically closed at that point.
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        key={index}
+                        title={item.name}
+                        subtitle={item.description}
+                        onPress={() => navigate('DishDetail', { dishId: item.id })}
+                        leftAvatar={{ source: {uri: baseUrl + item.image}}}
                     />
+                </Swipeout>
             );
         };
 
+        
         if (this.props.dishes.isLoading) {
             return(
                 <Loading />
@@ -61,4 +82,4 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
--- redux/favorites.js
+++ redux/favorites.js
@@ -8,6 +8,10 @@ export const favorites = (state = [], action) => {
             else
                 return state.concat(action.payload);
                 
+                case ActionTypes.DELETE_FAVORITE:
+                return state.filter((favorite) => favorite !== action.payload);//from the state, we will filter out that payload,
+                //that dish that corresponds to that payload. And so that dish will be removed from that state over here. And then that modified state is returned from this action here.
+
         default:
           return state;
       }
