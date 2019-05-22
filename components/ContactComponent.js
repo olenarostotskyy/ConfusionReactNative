1d9fd771d02b9fa173dcaab8a136c7f019630e16
Olena Rostotskyy <olenazachkow@gmail.com>
2019-05-22 13:13:59 -0700

    Sending email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { ScrollView, Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
-
+import { MailComposer } from 'expo';
 
 class Contact extends Component {
-    constructor(props) {
-        super(props);
+    
+    
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['confusion@food.net'],
+            subject: 'Enquiry',
+            body: 'To whom it may concern:'
+        })
     }
 
     static navigationOptions = {//f you want to further customize for each of the components in your navigator, you can specify the navigation options like this inside the component. 
@@ -21,25 +27,30 @@ class Contact extends Component {
                 <Card
                     title='Contact information'>
 
-                    <Text>
+                    <Text style ={{margin: 10}}>
                         121, Clear Water Bay Road
                       </Text>
-                      <Text>
+                      <Text style ={{margin: 10}}>
                         Clear Water Bay, Kowloon
                         </Text>
-                        <Text>
+                        <Text style ={{margin: 10}}>
                         HONG KONG
                         </Text>
-                        <Text>
+                        <Text style ={{margin: 10}}>
                         Tel: +852 1234 5678
                         </Text>
-                        <Text>
+                        <Text style ={{margin: 10}}>
                         Fax: +852 8765 4321
                         </Text>
-                        <Text>
+                        <Text style ={{margin: 10}}>
                         Email:confusion@food.net
                         </Text>
-
+                        <Button
+                        title="Send Email"
+                        buttonStyle={{backgroundColor: "#512DA8"}}
+                        icon={<Icon name='envelope-o' type='font-awesome' color='white' />}
+                        onPress={this.sendMail}
+                        />
                 </Card>
                 </Animatable.View>
             </ScrollView>
